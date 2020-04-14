14d5f97856574fc564e053ce5feb07ac3d9e3c5a
Fatih Doğan <fatih@fatihdogan.dev>
2020-04-14 22:54:01 +0300

    Clean up code by using promise

--- background.js
+++ background.js
@@ -21,116 +21,139 @@ const CLOSE_TABS_FROM_THIS_DOMAIN_ACTION = {
 
 const ALL_ACTIONS = [MERGE_AND_SORT_ACTION, MERGE_ACTION, SORT_ACTION, CLOSE_TABS_FROM_THIS_DOMAIN_ACTION]
 
-function baseAction(actionId, ignorePinnedTabs) {
+function getSelectedTab() {
+  return new Promise(function (resolve, reject) {
+    chrome.tabs.getSelected(tab => {
+      resolve(tab)
+    })
+  })
+}
+
+function getCurrentWindow() {
+  return new Promise(function (resolve, reject) {
+    chrome.windows.getCurrent({ "populate": true }, function (currentWindow) {
+      resolve(currentWindow)
+    })
+  })
+}
+
+function getIsPinnedTabsIgnored() {
+  return new Promise(function (resolve, reject) {
+    chrome.storage.sync.get({ ignorePinnedTabs: true }, storage => {
+      resolve(storage.ignorePinnedTabs)
+    })
+  })
+}
+
+function getAllWindows() {
+  return new Promise(function (resolve, reject) {
+    chrome.windows.getAll({ "populate": true }, function (windows) {
+      resolve(windows)
+    })
+  })
+}
+
+function getTabsOfWindow(window, isPinnedTabsIgnored) {
+  return window.tabs.filter(tab => { return !isPinnedTabsIgnored || !tab.pinned })
+}
+
+function getTabsOfWindows(windows, isPinnedTabsIgnored) {
+  let tabs = []
+  windows.forEach(window => {
+    getTabsOfWindow(window, isPinnedTabsIgnored).forEach(tab => {
+      tabs.push(tab)
+    })
+  })
+  return tabs
+}
+
+async function getAllTabs() {
+  let isPinnedTabsIgnored = await getIsPinnedTabsIgnored()
+  let windows = await getAllWindows()
+  return getTabsOfWindows(windows, isPinnedTabsIgnored)
+}
+
+async function getCurrentWindowTabs() {
+  let isPinnedTabsIgnored = await getIsPinnedTabsIgnored()
+  let currentWindow = await getCurrentWindow()
+  return getTabsOfWindow(currentWindow, isPinnedTabsIgnored)
+}
+
+function baseAction(actionId) {
   if (actionId == MERGE_AND_SORT_ACTION.id) {
-    mergeWindowsAndSortTabsAction(ignorePinnedTabs)
+    mergeWindowsAndSortTabsAction()
   } else if (actionId == MERGE_ACTION.id) {
-    mergeWindowsAction(ignorePinnedTabs)
+    mergeWindowsAction()
   } else if (actionId == SORT_ACTION.id) {
-    sortTabsAction(ignorePinnedTabs)
+    sortTabsAction()
   } else if (actionId == CLOSE_TABS_FROM_THIS_DOMAIN_ACTION.id) {
     closeTabsFromCurrentDomainAction()
   }
 }
 
-function mergeWindowsAndSortTabsAction(ignorePinnedTabs) {
-  mergeWindows(ignorePinnedTabs, _ => {
-    sortTabs(ignorePinnedTabs)
+async function mergeWindowsAndSortTabsAction(ignorePinnedTabs) {
+  await mergeWindows()
+  await sortTabs()
+}
+
+async function mergeWindowsAction(ignorePinnedTabs) {
+  await mergeWindows()
+}
+
+async function sortTabsAction(ignorePinnedTabs) {
+  await sortTabs()
+}
+
+async function closeTabsFromCurrentDomainAction() {
+  let selectedTab = await getSelectedTab()
+  let domain = new URL(selectedTab.url).host
+  let tabs = await getAllTabs()
+  tabs = tabs.filter(tab => { return new URL(tab.url).host == domain })
+  tabs.forEach(tab => {
+    chrome.tabs.remove(tab.id)
   })
 }
 
-function mergeWindowsAction(ignorePinnedTabs) {
-  mergeWindows(ignorePinnedTabs, _ => { })
-}
+async function mergeWindows() {
+  let currentWindow = await getCurrentWindow()
+  let tabs = await getAllTabs()
 
-function sortTabsAction(ignorePinnedTabs) {
-  sortTabs(ignorePinnedTabs)
-}
-
-function closeTabsFromCurrentDomainAction() {
-  chrome.tabs.getSelected(tab => {
-    var url = new URL(tab.url)
-    closeTabsFromDomain(url.host)
-  })
-}
-
-function mergeWindows(ignorePinnedTabs, callback) {
-  chrome.windows.getCurrent({ "populate": true }, function (currentWindow) {
-    chrome.windows.getAll({ "populate": true }, function (windows) {
-      tabs = []
-      windows.forEach(window => {
-        if (window.type != "popup") {
-          window.tabs.forEach(tab => {
-            if (ignorePinnedTabs == false || tab.pinned != true) {
-              tabs.push(tab)
-            }
-          })
-        }
-      })
-      tabs.forEach(tab => {
-        chrome.tabs.move(tab.id,
-          { "windowId": currentWindow.id, "index": tabs.length * 2 })
-        if (tab.pinned == true) {
-          chrome.tabs.update(tab.id, { "pinned": true })
-        }
-      })
-      callback()
-    })
-  })
-}
-
-function sortTabs(ignorePinnedTabs) {
-  chrome.windows.getCurrent({ "populate": true }, function (currentWindow) {
-    var tabs = currentWindow.tabs
-    if (ignorePinnedTabs) {
-      tabs = tabs.filter(tab => { return !tab.pinned })
+  tabs.forEach(tab => {
+    chrome.tabs.move(tab.id, { "windowId": currentWindow.id, "index": tabs.length * 2 })
+    if (tab.pinned == true) {
+      chrome.tabs.update(tab.id, { "pinned": true })
     }
-    tabs.sort(function (a, b) {
-      if (a.url < b.url) {
-        return -1
-      } else if (a.url > b.url) {
-        return 1
-      } else {
-        return 0
-      }
-    })
-    tabs.forEach(tab => {
-      chrome.tabs.move(tab.id,
-        { "windowId": currentWindow.id, "index": tabs.length * 2 })
-      if (tab.pinned == true) {
-        chrome.tabs.update(tab.id, { "pinned": true })
-      }
-    })
   })
 }
 
-function closeTabsFromDomain(domain) {
-  chrome.windows.getAll({ "populate": true }, function (windows) {
-    tabs = []
-    windows.forEach(window => {
-      if (window.type != "popup") {
-        window.tabs.forEach(tab => {
-          if (tab.pinned != true) {
-            var url = new URL(tab.url)
-            if (url.host == domain) {
-              chrome.tabs.remove(tab.id)
-            }
-          }
-        })
-      }
-    })
-  });
+async function sortTabs() {
+  let currentWindow = await getCurrentWindow()
+  let tabs = await getCurrentWindowTabs()
+  tabs.sort(function (a, b) {
+    if (a.url < b.url) {
+      return -1
+    } else if (a.url > b.url) {
+      return 1
+    } else {
+      return 0
+    }
+  })
+  tabs.forEach(tab => {
+    chrome.tabs.move(tab.id,
+      { "windowId": currentWindow.id, "index": tabs.length * 2 })
+    if (tab.pinned == true) {
+      chrome.tabs.update(tab.id, { "pinned": true })
+    }
+  })
 }
 
 chrome.contextMenus.onClicked.addListener(event => {
-  chrome.storage.sync.get({ ignorePinnedTabs: true }, result => {
-    baseAction(event.menuItemId, result.ignorePinnedTabs)
-  })
+  baseAction(event.menuItemId)
 });
 
 chrome.browserAction.onClicked.addListener(event => {
-  chrome.storage.sync.get({ defaultAction: MERGE_AND_SORT_ACTION.id, ignorePinnedTabs: true }, result => {
-    baseAction(result.defaultAction, result.ignorePinnedTabs)
+  chrome.storage.sync.get({ defaultAction: MERGE_AND_SORT_ACTION.id }, result => {
+    baseAction(result.defaultAction)
   })
 })
 
